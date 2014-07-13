478968255d2b86b09ee9183969488e699f27e868
Ryan <rsul.dev@me.com>
2014-07-13 19:12:15 -0400

    Add array-reverse php

---
+++ array-reverse/php/main.php
@@ -0,0 +1,31 @@
+<?php
+
+if ($argc != 3)
+{
+	printf("Usage: %s <size> <iterations>", $argv[0]);
+	exit(1);
+}
+
+
+$myList = range(0, $argv[1]);
+
+for ($i = 0; $i < $argv[2]; $i++)
+{
+	reverseArray($array);
+}
+
+/**
+ * Reverses an array inplace
+ * @param array &$array An array to be reversed
+ */
+function reverseArray(&$array)
+{
+	for ($i = 0; $i < count($array) >> 1; $i++)
+	{
+		$lastIdx = count($array) - $i - 1;
+		$lastValue = $array[$lastIdx];
+		$array[$lastIdx] = $array[$i];
+		$array[$i] = $lastValue;
+	}
+}
+
